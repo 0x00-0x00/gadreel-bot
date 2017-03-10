b08478a4fc25911e36195a5e8cae5942da71328d
zero-cool <andre.marques@fatec.sp.gov.br>
2017-03-10 08:48:38 -0300

    Adding talking sets
    For more versatile talking

---
+++ src/talk.py
@@ -0,0 +1,20 @@
+image_download = [
+    "Imagem baixada com sucesso! ['{0}']",
+    "Terminei de baixar a imagem. ['{0}']",
+    "Obrigado, baixei sua imagem. ['{0}']",
+    "Download de imagem completo. ['{0}']"
+]
+
+file_download = [
+    "Arquivo baixado com sucesso! ['{0}']",
+    "Terminei de baixar o arquivo. ['{0}']",
+    "Obrigado, baixei seu arquivo. ['{0}']",
+    "Download de arquivo completo. ['{0}']"
+]
+
+video_download = [
+    "Video baixado com sucesso! ['{0}']",
+    "Terminei de baixar o video. ['{0}']",
+    "Obrigado, baixei seu video. ['{0}']",
+    "Download de video completo. ['{0}']"
+]
