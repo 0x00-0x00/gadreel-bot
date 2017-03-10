e933885e4f981a2d12d74912b9a7eade5d111ec4
zero-cool <andre.marques@fatec.sp.gov.br>
2017-03-10 15:18:48 -0300

    Markdown for messages
    Adding markdown support

--- src/talk.py
+++ src/talk.py
@@ -1,27 +1,27 @@
 image_download = [
-    "Imagem baixada com sucesso! ['{0}']",
-    "Terminei de baixar a imagem. ['{0}']",
-    "Obrigado, baixei sua imagem. ['{0}']",
-    "Download de imagem completo. ['{0}']"
+    "Imagem baixada com sucesso! \n_{0}_",
+    "Terminei de baixar a imagem. \n_{0}_",
+    "Obrigado, baixei sua imagem. \n_{0}_",
+    "Download de imagem completo. \n_{0}_"
 ]
 
 file_download = [
-    "Arquivo baixado com sucesso! ['{0}']",
-    "Terminei de baixar o arquivo. ['{0}']",
-    "Obrigado, baixei seu arquivo. ['{0}']",
-    "Download de arquivo completo. ['{0}']"
+    "Arquivo _{0}_ baixado com sucesso!",
+    "Terminei de baixar o arquivo. \n_{0}_",
+    "Obrigado, baixei seu arquivo. \n_{0}_",
+    "Download de arquivo completo. \n_{0}_"
 ]
 
 video_download = [
-    "Video baixado com sucesso! ['{0}']",
-    "Terminei de baixar o video. ['{0}']",
-    "Obrigado, baixei seu video. ['{0}']",
-    "Download de video completo. ['{0}']"
+    "Video baixado com sucesso! \n_{0}_",
+    "Terminei de baixar o video. \n_{0}_",
+    "Obrigado, baixei seu video. \n_{0}_",
+    "Download de video completo. \n_{0}_"
 ]
 
 cant_find_tasks = [
-    "Nao encontrei nenhuma tarefa.",
+    "Nao encontrei *nenhuma* tarefa.",
     "Nao foram encontradas tarefas.",
-    "Nenhuma tarefa foi encontrada.",
-    "Nao existe tarefas."
+    "*Nenhuma* tarefa foi encontrada.",
+    "Nao existe tarefas. Parabens jovi."
 ]
