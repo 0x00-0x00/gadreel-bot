58c62fad42f0605fc0cd4f89f77541a226239501
Z3R0C00L <andre.marques@fatec.sp.gov.br>
2017-04-20 13:42:30 -0300

    Added regex code to encoding/decoding functions

--- src/b64decoder.py
+++ src/b64decoder.py
@@ -3,7 +3,7 @@ import base64
 
 
 def b64decode_regex(message):
-    regex = ""
+    regex = "^\/b64[d]\s(?P<data>[a-zA-Z0-9\=]+)"
     m = re.match(regex, message)
     if not m:
         return None
@@ -11,7 +11,7 @@ def b64decode_regex(message):
         return True
 
 def b64encode_regex(message):
-    regex = ""
+    regex = "^\/b64[e]\s(?P<data>[a-zA-Z0-9\=]+)"
     m = re.match(regex, message)
     if not m:
         return None
@@ -19,9 +19,19 @@ def b64encode_regex(message):
         return True
 
 def b64d(message):
-    string_to_decode = message.split(" ")[:-1]
-    return base64.b64decode(string_to_decode)
+    regex = "^\/b64[de]\s(?P<data>[a-zA-Z0-9\=]+)"
+    m = re.match(regex, message)
+    if not m:
+        return None
+    else:
+        decoded_string = base64.b64decode(m["data"])
+        return base64.b64decode(decoded_string)
 
 def b64e(message):
-    string_to_encode = message.split(" ")[:-1]
-    return base64.b64encode(string_to_encode)
+    regex = "^\/b64[de]\s(?P<data>[a-zA-Z0-9\=]+)"
+    m = re.match(regex, message)
+    if not m:
+        return None
+    else:
+        encoded_string = base64.b64encode(m["data"])
+        return base64.b64decode(encoded_string)
