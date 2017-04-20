6c3490fc057b47cf037a980779d44bf0520bc5a5
Z3R0C00L <andre.marques@fatec.sp.gov.br>
2017-04-20 09:13:15 -0300

    adding authentication module

--- src/authentication.py
+++ src/authentication.py
@@ -1,5 +1,106 @@
+from shemutils.logger import Logger
 from telemanager.database import *
-master_id = 193665372
+from time import time
+import re
+master_id = 193665372  # the master ID -- configure it by hand to ensure security
+
+logger = Logger("tele-manager")
+
+class MemberRegexHandler(object):
+    def __init__(self, message):
+        self.message = message
+        self._parse_regex()
+
+    def _parse_regex(self):
+        req = self._request_regex()
+        acc = self._accept_regex()
+
+        #  Returns if no match.
+        if not acc and not req:
+            return 0
+
+        if req:
+            obj = RequestMember(req["id"])
+            if obj.status is True:
+                logger.debug("User #{0} request successfully added.".format(req["id"]))
+                return 0
+            else:
+                return -1
+
+        if acc:
+            obj = AcceptMember(req["id"])
+            if obj.status is True:
+                logger.debug("User #{0} acceptance successfully done.".format(req["id"]))
+                return 0
+            else:
+                return -1
+
+    def _request_regex(self):
+        """
+        Tries to identify the message using one regular expression.
+        """
+        regex = "^(register?|registrar)\s+(?P<id>[\d]+)"
+        m = re.match(regex, self.message.decode())
+        if not m:
+            return None
+        return m.groupdict()
+
+    def _accept_regex(self):
+        """
+        Tries to identify the message using one regular expression.
+        """
+        regex = "^(acc?|accept?|aceitar)\s+(?P<id>[\d]+)"
+        m = re..match(regex, self.message.decode())
+        if not m:
+            return None
+        return m.groupdict()
+
+class RequestMember(object):
+    """
+    Object to realize member requesting.
+    """
+    def __init__(self, id):
+        self.id = int(id)
+        self.status = False
+        if self._register_new_member() == 0:
+            self.status = False
+
+    def _register_new_member(self):
+        """
+        Creates a row into database with 'PENDING' status
+        """
+        try:
+            db.controller.execute(t4.insert_data([self.id, "PENDING", str(time())]))
+            db.save()
+        except Exception as e:
+
+        return 0
+
+
+class AcceptMember(object):
+    """
+    Object to realize member acceptance.
+    """
+    def __init__(self, id):
+        self.id = id
+        self._accept_member()
+
+    def _accept_member(self):
+        """
+        Accept a member from the MEMBERS table which status is 'PENDING'
+        """
+        search_SQL = t4.search("USER_ID", self.id)
+        db.controller.execute(search_SQL)
+        results = db.controller.get()
+        if len(results) == 0:
+            return -1
+        else:
+            for result in results:
+                user_id, status, timestamp = result[1], result[2], result[3]
+                if status == "PENDING":
+                    update_SQL = t4.update()
+        return 0
+
 
 class AuthenticationCheck(object):
     """
