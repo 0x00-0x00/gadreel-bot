1d447a4748ed63d453f8d6774a0ed29973154188
Z3R0C00L <andre.marques@fatec.sp.gov.br>
2017-04-22 12:31:53 -0300

    help me message

---
+++ src/helpme.py
@@ -0,0 +1,45 @@
+import re
+
+
+def help_regex(message):
+    regex = "^\/help"
+    m = re.match(regex, message)
+    if not m:
+        return None
+    else:
+        return True
+    return 0
+
+HELP_MARKDOWN = """*Para procurar/baixar material ou mensagens*:
+procurar "PALAVRA-CHAVE"
+procurar "ARQUIVO_PARA_BAIXAR.PDF"
+
+*Para pesquisar na internet*:
+/duckduck PESQUISA
+
+*Para administrar tarefas*:
+criar tarefa "Nome da Tarefa"
+del tarefa "83fab1d3...."
+listar tarefas | list tasks
+
+*Habilidades de CTF*:
+Base64 En/Decoder
+/b64e TEXTO
+/b64d BASE64
+
+Morse En/Decoder:
+/morsee PLAINTEXT
+/morsee ``MORSE_CODE``
+
+Ascii En/Decoder:
+/asciie TEXTO
+/asciid NN NN NN NN
+
+Caesar Cipher En/Decryptor:
+/caesare PLAINTEXT
+/caesard CIPHER
+
+Vignere Cipher En/Decryptor:
+/vigneree "PLAINTEXT" "KEY"
+/vignered "CIPHER" "KEY"
+"""
